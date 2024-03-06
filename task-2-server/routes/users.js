67ac39e0af3b2d396aa6121f24516fec0f8e3b1e
mbganesh <mbganesh@gmail.com>
2024-03-06 22:03:34 +0530

    task1 completed

--- task-2-server/routes/users.js
+++ task-2-server/routes/users.js
@@ -1,9 +1,54 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var UserModel = require("../model/userSchema");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("User routes");
+});
+
+router.get("/get-count", async (req, res, next) => {
+  try {
+    let data = await UserModel.findOne({});
+    console.log(data);
+    res.json({
+      status: true,
+      data: data,
+    });
+  } catch (err) {
+    res.json({
+      status: true,
+      message: "Err-" + err.message,
+    });
+  }
+});
+
+router.post("/add-item", async (req, res, next) => {
+  try {
+    let { name } = req.body;
+    console.log(name, "name");
+
+    const updatedData = await UserModel.findByIdAndUpdate(
+      {},
+      {
+        $push: { names: name },
+        $inc: { addCount: 1 },
+      },
+      { new: true }
+    );
+
+    let resp = {
+      status: true,
+      message: "Item Added Successfully",
+      data: updatedData,
+    };
+    console.table(resp);
+    res.json(resp);
+  } catch (err) {
+    res.json({
+      status: true,
+      message: "Err-" + err.message,
+    });
+  }
 });
 
 module.exports = router;
