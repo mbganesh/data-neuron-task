fe50a2051af9ace7fb7fe63fd50f263a2a99c07c
mbganesh <mbganesh@gmail.com>
2024-03-07 13:44:35 +0530

    Completed

--- task-2-server/routes/users.js
+++ task-2-server/routes/users.js
@@ -2,21 +2,27 @@ var express = require("express");
 var router = express.Router();
 var UserModel = require("../model/userSchema");
 
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Types;
+
 router.get("/", function (req, res, next) {
   res.send("User routes");
 });
 
-router.get("/get-count", async (req, res, next) => {
+router.get("/get-data", async (req, res, next) => {
   try {
     let data = await UserModel.findOne({});
     console.log(data);
+    if (!data) {
+      data = [];
+    }
     res.json({
       status: true,
-      data: data,
+      data,
     });
   } catch (err) {
     res.json({
-      status: true,
+      status: false,
       message: "Err-" + err.message,
     });
   }
@@ -24,31 +30,65 @@ router.get("/get-count", async (req, res, next) => {
 
 router.post("/add-item", async (req, res, next) => {
   try {
-    let { name } = req.body;
-    console.log(name, "name");
+    const { name } = req.body;
+    let storeData = {
+      value: name,
+      _id: new ObjectId(),
+    };
+    const isFirst = (await UserModel.countDocuments()) > 0;
+    if (isFirst) {
+      await UserModel.updateMany(
+        {},
+        {
+          $push: { names: storeData },
+          $inc: { addCount: 1 },
+        },
+        { new: true }
+      );
+    } else {
+      await UserModel.create({
+        names: [storeData],
+        addCount: 1,
+        updateCount: 0,
+      });
+    }
 
-    const updatedData = await UserModel.findByIdAndUpdate(
-      {},
-      {
-        $push: { names: name },
-        $inc: { addCount: 1 },
-      },
-      { new: true }
-    );
-
-    let resp = {
+    const resp = {
       status: true,
       message: "Item Added Successfully",
-      data: updatedData,
     };
-    console.table(resp);
     res.json(resp);
   } catch (err) {
     res.json({
-      status: true,
+      status: false,
       message: "Err-" + err.message,
     });
   }
 });
 
+router.post("/update-item", async (req, res, next) => {
+  try {
+    const { _id, name } = req.body;
+
+    await UserModel.findOneAndUpdate(
+      { "names._id": new ObjectId(_id) },
+      {
+        $set: { "names.$.value": name },
+        $inc: { updateCount: 1 },
+      },
+      { new: true }
+    );
+
+    const resp = {
+      status: true,
+      message: "Item Updated Successfully",
+    };
+    res.json(resp);
+  } catch (err) {
+    res.json({
+      status: false,
+      message: "Err-" + err.message,
+    });
+  }
+});
 module.exports = router;
--- task1-client/src/App.js
+++ task1-client/src/App.js
@@ -2,14 +2,22 @@ import TablePage from "pages/TablePage";
 import "./App.css";
 
 import Panels from "components/Panels";
+import { BrowserRouter, Route, Routes, } from "react-router-dom";
+import HomePage from "pages/HomePage";
 
 function App() {
   return (
-    <div>
-      <TablePage/>
-      <Panels/>
-      {/* <Basic /> */}
-    </div>
+    <>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<HomePage />} path="/" />
+            <Route element={<Panels />} path="/resize" />
+            <Route element={<TablePage />} path="/table" />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </>
   );
 }
 
